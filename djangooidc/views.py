d5cf763b392791a64d6968a0e3a15f183f050463
Steven Arroyo <>
2018-08-02 15:51:45 -0500

    https://docs.djangoproject.com/en/1.11/topics/auth/default/#django.contrib.auth.views.login

--- djangooidc/views.py
+++ djangooidc/views.py
@@ -7,7 +7,7 @@ from urllib.parse import urlencode
 from django.conf import settings
 from django.contrib.auth import logout as auth_logout, authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import login as auth_login_view, logout as auth_logout_view
+from django.contrib.auth.views import LoginView as auth_login_view, LogoutView as auth_logout_view
 from django.shortcuts import redirect, render, resolve_url
 from django.http import HttpResponse, HttpResponseRedirect
 from django import forms
